51f4e52d7b7ace9390c0e279c622104648d81580
cmr211 <bartosz.surma211@gmail.com>
2022-03-18 09:36:18 +0100

    Update: dividers

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,69 +1,63 @@
 gsap.registerPlugin(ScrollTrigger)
 
-function element(el) {
-    return document.getElementById(el)
-}
-
-function elements(els) {
-    return document.getElementsByClassName(els)
-}
-
-const section1scroll = gsap.timeline({
-    defaults: { duration: 0.3, ease: Power0.easeInOut },
-    scrollTrigger: {
-        trigger: "#section-1",
-        start: "top 80%",
-        end: "20% 10%",
-        toggleActions: ACTION_TYPE,
-    },
-})
-
-section1scroll
-    .from(elements("anim1a"), {
-        y: -10,
-        opacity: 0,
-        stagger: 0.4,
-    })
-    .from(elements("anim1b"), {
-        opacity: 0,
-    })
-
-// const section3scroll = gsap.timeline({
-//     defaults: { duration: 0.3, ease: Power0.easeInOut },
-//     scrollTrigger: {
-//         trigger: "#section-3",
-//         start: "top 80%",
-//         end: "bottom 2%",
-//         toggleActions: "play reset play reset",
-//     },
-// })
-
-// section3scroll
-//     .from(document.getElementById("section-3-container").children, {
-//         delay: 0.1,
-//         y: -10,
-//         opacity: 0,
-//         stagger: 0.2,
-//     }, "<")
-//     .from(document.getElementById("section-3-grid").children, {
-//         x: -10,
-//         opacity: 0,
-//         stagger: 0.3,
-//     }, "<")
-
-gsap.to(document.querySelector(".white-animation"), {
-    scrollTrigger: {
-        trigger: "#section-4",
-        start: "top 80%",
-        end: "bottom 10%",
-        toggleActions: ACTION_TYPE,
-        scrub: true,
-    },
-
-    height: 400,
-})
-
+section1animation()
+whiteAnimation()
 simpleAnimation()
+dividersAnimation()
+
+function dividersAnimation() {
+    const dividers = document.querySelectorAll(".divider")
+    dividers.forEach((divider, index) => {
+        gsap.to(divider, {
+            scrollTrigger: {
+                id: index+1,
+                trigger: divider,
+                start: "top 90%",
+                end: "bottom 10%",
+                // toggleActions: ACTION_TYPE,
+                scrub: 1,
+                markers: true,
+            },
+            transform: "scaleX(1)",
+        })
+    })
+}
+
+function section1animation() {
+    const section1scroll = gsap.timeline({
+        defaults: { duration: 0.3, ease: Power0.easeInOut },
+        scrollTrigger: {
+            trigger: "#section-1",
+            start: "top 80%",
+            end: "20% 10%",
+            toggleActions: ACTION_TYPE,
+        },
+    })
+    
+    section1scroll
+        .from(elements("anim1a"), {
+            y: -10,
+            opacity: 0,
+            stagger: 0.4,
+        })
+        .from(elements("anim1b"), {
+            opacity: 0,
+        })
+}
+
+function whiteAnimation() {
+    gsap.to(document.querySelector(".white-animation"), {
+        scrollTrigger: {
+            trigger: "#section-4",
+            start: "top 80%",
+            end: "bottom 10%",
+            toggleActions: ACTION_TYPE,
+            scrub: true,
+        },
+    
+        height: 400,
+    })
+}
 
 function simpleAnimation() {
     const elements = document.querySelectorAll(".anim-simple")
@@ -85,4 +79,12 @@ function simpleAnimation() {
             duration: 0.4,
         })
     })
+}
+
+function element(el) {
+    return document.getElementById(el)
+}
+
+function elements(els) {
+    return document.getElementsByClassName(els)
 }
